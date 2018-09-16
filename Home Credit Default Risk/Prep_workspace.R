8cffb94e63aa234615ba7a89828c6df53ee88cb5
claudio <34164395+claudio1975@users.noreply.github.com>
2018-09-16 10:51:58 +0200

    Update Prep_workspace.R

--- Home Credit Default Risk/Prep_workspace.R
+++ Home Credit Default Risk/Prep_workspace.R
@@ -20,16 +20,16 @@ h2o.init(ip = "localhost",
 
 cat("Loading data...\n")
 
-bbalance <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/bureau_balance.csv",header = T, showProgress = F)
-bureau <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/bureau.csv",header = T, showProgress = F)
-cc_balance <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/credit_card_balance.csv",header = T, showProgress = F)
-payments <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/installments_payments.csv",header = T, showProgress = F) 
-pc_balance <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/POS_CASH_balance.csv",header = T, showProgress = F)
-prev <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/previous_application.csv",header = T, showProgress = F)
-train <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/application_train.csv",header = T, showProgress = F) 
-test <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/application_test.csv",header = T, showProgress = F)
-sub <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/sample_submission.csv",header = T, showProgress = F)
-descr <- fread("C:/Users/User/Documents/Kaggle/Home Credit Default Risk/HomeCredit_columns_description.csv",header = T, showProgress = F)
+bbalance <- fread(".../Documents/Kaggle/Home Credit Default Risk/bureau_balance.csv",header = T, showProgress = F)
+bureau <- fread(".../Documents/Kaggle/Home Credit Default Risk/bureau.csv",header = T, showProgress = F)
+cc_balance <- fread(".../Documents/Kaggle/Home Credit Default Risk/credit_card_balance.csv",header = T, showProgress = F)
+payments <- fread(".../Documents/Kaggle/Home Credit Default Risk/installments_payments.csv",header = T, showProgress = F) 
+pc_balance <- fread(".../Documents/Kaggle/Home Credit Default Risk/POS_CASH_balance.csv",header = T, showProgress = F)
+prev <- fread(".../Documents/Kaggle/Home Credit Default Risk/previous_application.csv",header = T, showProgress = F)
+train <- fread(".../Documents/Kaggle/Home Credit Default Risk/application_train.csv",header = T, showProgress = F) 
+test <- fread(".../Documents/Kaggle/Home Credit Default Risk/application_test.csv",header = T, showProgress = F)
+sub <- fread(".../Documents/Kaggle/Home Credit Default Risk/sample_submission.csv",header = T, showProgress = F)
+descr <- fread(".../Documents/Kaggle/Home Credit Default Risk/HomeCredit_columns_description.csv",header = T, showProgress = F)
 
 #-----------------------------------------
 
